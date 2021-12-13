3ba6430de5e9ae7038159c63994efa49d0a48260
Christian Foster <fosterchris000@gmail.com>
2021-12-13 16:17:30 -0500

    Created a header and passed a prop called text.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
+import Header from "./components/Header";
 function App() {
-  const title = "Blog post";
-  const body = "This is my blog post ";
-  const comments = [
-    { id: 1, text: "comment one" },
-    { id: 2, text: "comment two" },
-    { id: 3, text: "comment three" },
-  ];
-  const loading = false;
-  if (loading) return <h1>Loading...</h1>;
-  const showComments = true;
-
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
-    <div classname="container">
-      <h1>My App</h1>
-    </div>
+    <>
+      <Header text={true} />
+      <div classname="container">
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
