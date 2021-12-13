751bb1423406f93e8315d5149d00db8e87ccd6fb
Christian Foster <fosterchris000@gmail.com>
2021-12-13 15:55:30 -0500

    Created comments example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 function App() {
-  return <h1>Hello From the App Component</h1>;
+  const title = "Blog post";
+  const body = "This is my blog post ";
+  const comments = [
+    { id: 1, text: "comment one" },
+    { id: 2, text: "comment two" },
+    { id: 3, text: "comment three" },
+  ];
+  return (
+    <div classname="container">
+      <h1>{title} </h1>
+      <p>{body} </p>
+      <p>{Math.random() * (5 + 5)} </p>
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
