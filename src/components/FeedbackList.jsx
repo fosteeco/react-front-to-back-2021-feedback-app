a2812f54707f2256f589bbd8432a103b5aabfc43
Christian Foster <fosterchris000@gmail.com>
2021-12-13 17:15:37 -0500

    Created card styling component. Also setup propType checking for components

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,6 @@
 import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
+
 function FeedbackList({ feedback }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback yet</p>;
@@ -11,5 +13,15 @@ function FeedbackList({ feedback }) {
     </div>
   );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;
