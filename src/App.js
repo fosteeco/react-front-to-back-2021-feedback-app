c8fd3a9461b062858a16ce40ca5982c081649e93
Christian Foster <fosterchris000@gmail.com>
2021-12-13 16:11:05 -0500

    Separated comment code into variables

--- src/App.js
+++ src/App.js
@@ -6,17 +6,27 @@ function App() {
     { id: 2, text: "comment two" },
     { id: 3, text: "comment three" },
   ];
-  return (
-    <div classname="container">
-      <h1>{title} </h1>
-      <p>{body} </p>
-      <p>{Math.random() * (5 + 5)} </p>
+  const loading = false;
+  if (loading) return <h1>Loading...</h1>;
+  const showComments = true;
+
+  const commentBlock = (
+    <div className="comments">
       <h3>Comments ({comments.length})</h3>
       <ul>
         {comments.map((comment, index) => (
           <li key={index}>{comment.text}</li>
         ))}
       </ul>
+    </div>
+  );
+
+  return (
+    <div classname="container">
+      <h1>{title} </h1>
+      <p>{body} </p>
+      {showComments && commentBlock}
+      <p>{Math.random() * (5 + 5)} </p>
     </div>
   );
 }
